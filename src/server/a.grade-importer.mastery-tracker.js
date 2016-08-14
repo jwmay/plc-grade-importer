bc70e7915d9ca3897d389958abf664f5bc7e44ab
Joseph May <jwmay87@gmail.com>
2016-08-14 12:50:00 -0700

    add mastery tracker object and unit tests

---
+++ src/server/a.grade-importer.mastery-tracker.js
@@ -0,0 +1,42 @@
+// Copyright 2016 Joseph W. May. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
+/**
+ * An object representing the Mastery Tracker spreadsheet. Inherits from the
+ * BaseSpreadsheet class.
+ */
+var MasteryTracker = function(config) {
+  BaseSpreadsheet.call(this, config);
+};
+inherit_(MasteryTracker, BaseSpreadsheet);
+
+
+/**
+ * Return true if the Mastery Tracker spreadsheet is properly configured with
+ * the correct sheets. The verification is done based on the sheet names present
+ * in the spreadsheet.
+ * 
+ * @return {boolean} True if the spreadsheet is properly configured.
+ */
+MasteryTracker.prototype.isConfigured = function() {
+  var config = Configuration.getCurrent();
+  var configSheets = config.sheets;
+  for (var sheet in configSheets) {
+    if (this.hasSheet(configSheets[sheet].name) === false) {
+      return false;
+    }
+  }
+  return true;
+};
---
+++ src/tests/a.grade-importer.tests.mastery-tracker.js
@@ -0,0 +1,92 @@
+// Copyright 2016 Joseph W. May. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
+/**
+ * Base class for testing the Mastery Tracker class.
+ */
+var MasteryTrackerTests = function() {
+  Tests.call(this);
+};
+inherit_(MasteryTrackerTests, Tests);
+
+
+/**
+ * Test to confirm that the configurations are properly loaded.
+ */
+MasteryTrackerTests.prototype.testConfigLoad = function() {
+  var actualSpreadsheetId = SPREADSHEET_ID;
+  var testSpreadsheet = new MasteryTracker();
+  var testSpreadsheetId = testSpreadsheet.config.debugSpreadsheetId;
+  assertEquals_(actualSpreadsheetId, testSpreadsheetId);
+};
+
+
+/**
+ * Test to confirm that the active spreadsheet is loaded.
+ */
+MasteryTrackerTests.prototype.testSpreadsheetLoad = function() {
+  var actualSpreadsheetName = SPREADSHEET_NAME;
+  var testSpreadsheet = new MasteryTracker().getSpreadsheet();
+  var testSpreadsheetName = testSpreadsheet.getName();
+  assertEquals_(actualSpreadsheetName, testSpreadsheetName);
+};
+
+
+/**
+ * Test to confirm that the currentUserIsOwner method correctly identifies
+ * the email address of the current user and spreadsheet owner.
+ */
+MasteryTrackerTests.prototype.testCurrentUserIsOwner = function() {
+  var testSpreadsheet = new MasteryTracker();
+  assertTrue_(testSpreadsheet.currentUserIsOwner(),
+          'The current user is not the owner.');
+};
+
+
+/**
+ * Test to confirm that the getSheetById method correclty selects the sheet
+ * wit the given sheetId.
+ */
+MasteryTrackerTests.prototype.testGetSheetById = function() {
+  var actualSheetName = 'Class Schedule';
+  var actualSheetId = 680820455;
+  var testSpreadsheet = new MasteryTracker();
+  var testSheet = testSpreadsheet.getSheetById(actualSheetId);
+  var testSheetName = testSheet.getName();
+  assertEquals_(actualSheetName, testSheetName);
+};
+
+
+/**
+ * Test to confirm that the getUniqueSheetName method provides a correct
+ * sheet name for a new sheet with the same name as an existing sheet.
+ */
+MasteryTrackerTests.prototype.testGetUniqueSheetName = function() {
+  var baseName = 'Class Schedule';
+  var actualSheetName = 'Class Schedule 2';
+  var testSpreadsheet = new MasteryTracker();
+  var testSheetName = testSpreadsheet.getUniqueSheetName(baseName);
+  assertEquals_(actualSheetName, testSheetName);
+};
+
+
+/**
+ * Test to confirm that the spreadsheet is properly configured.
+ */
+MasteryTrackerTests.prototype.testIsConfigured = function() {
+  var testSpreadsheet = new MasteryTracker();
+  assertTrue_(testSpreadsheet.isConfigured(),
+          'The spreadsheet is not properly configured.');
+};
