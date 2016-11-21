cc39cc4f4eb405a852ca53bfbf4f07ed521377a4
Joseph May <jwmay87@gmail.com>
2016-11-21 11:26:04 -0800

    limit scope of add-on in production code

--- src/environments/prd/a.grade-importer.prd.config.js
+++ src/environments/prd/a.grade-importer.prd.config.js
@@ -14,5 +14,4 @@
 
 // For production, this script will now only be able to act on Spreadsheets that
 // it is attached to via a user installing and activating the add-on.
-/** @OnlyCurrentDoc */
-// Put additional production configuration here
+@OnlyCurrentDoc
